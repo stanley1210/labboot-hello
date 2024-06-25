85da4e91799e5dbf14415b78667beebe41bf56c8
stanley1210 <stbs1210@gmail.com>
2024-06-25 14:54:32 +0800

    TEST1

--- src/main/java/tw/com/ispan/controller/Controller1.java
+++ src/main/java/tw/com/ispan/controller/Controller1.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-@RestController //@Controller+@ResponseBody
+@RestController // @Controller+@ResponseBody
 public class Controller1 {
     @PostMapping(path={"/ajax/controller1/method1/{pk}", "/ajax/controller1/method1"})    
     public String handlerMethod1(@PathVariable(required = false) Integer pk, @RequestBody(required = false) String body) {
@@ -20,7 +18,7 @@ public class Controller1 {
         JSONObject result =  new JSONObject()
                 .put("id", 10)
                 .put("name", "hahaha")
-                .put("price", 12.34)
+                .put("price", 12.34)GGGT
                 .put("make", "2024-06-18")
                 .put("Expire", 567)
                 ;

b6035d4a0e6715dd418d5f985791752d8d2a94fd
stanley1210 <stbs1210@gmail.com>
2024-06-25 14:53:09 +0800

    AAA

---
+++ src/main/java/tw/com/ispan/LabbootHelloApplication.java
@@ -0,0 +1,13 @@
+package tw.com.ispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbootHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabbootHelloApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tw/com/ispan/controller/Controller1.java
@@ -0,0 +1,30 @@
+package tw.com.ispan.controller;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController //@Controller+@ResponseBody
+public class Controller1 {
+    @PostMapping(path={"/ajax/controller1/method1/{pk}", "/ajax/controller1/method1"})    
+    public String handlerMethod1(@PathVariable(required = false) Integer pk, @RequestBody(required = false) String body) {
+        System.out.println("pk="+pk);
+        System.out.println("body="+body);
+        System.out.println("----------");
+
+        JSONObject result =  new JSONObject()
+                .put("id", 10)
+                .put("name", "hahaha")
+                .put("price", 12.34)
+                .put("make", "2024-06-18")
+                .put("Expire", 567)
+                ;
+        return result.toString();
+    }
+
+}
